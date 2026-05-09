991673ae7505858252a6a99cd15d2ea7f99b72b3
Faiz Ahamad <faizahmad788p@gmail.com>
2026-05-09 15:58:04 +0530

    fix cors for production

--- dacby-assignment/backend/server.js
+++ dacby-assignment/backend/server.js
@@ -3,34 +3,79 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
-const scrapeStories = require("./services/scraper");
-const authRoutes = require("./routes/authRoutes");
-const storyRoutes = require("./routes/storyRoutes");
+const scrapeStories =
+  require("./services/scraper");
+
+const authRoutes =
+  require("./routes/authRoutes");
+
+const storyRoutes =
+  require("./routes/storyRoutes");
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+
+// ✅ CORS
+
+app.use(cors({
+  origin: "*",
+}));
+
+
+// ✅ Middleware
+
 app.use(express.json());
+
+
+// ✅ MongoDB
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+
+  .then(() =>
+    console.log("MongoDB Connected")
+  )
+
+  .catch((err) =>
+    console.log(err)
+  );
+
+
+// ✅ Default Route
 
 app.get("/", (req, res) => {
+
   res.send("API Running");
+
 });
 
-app.use("/api/auth", authRoutes);
 
-app.use("/api/stories", storyRoutes);
+// ✅ Routes
 
-const PORT = process.env.PORT || 5000;
+app.use(
+  "/api/auth",
+  authRoutes
+);
+
+app.use(
+  "/api/stories",
+  storyRoutes
+);
+
+
+// ✅ Server
+
+const PORT =
+  process.env.PORT || 5000;
 
 app.listen(PORT, async () => {
-  console.log(`Server running on ${PORT}`);
+
+  console.log(
+    `Server running on ${PORT}`
+  );
 
   await scrapeStories();
+
 });
